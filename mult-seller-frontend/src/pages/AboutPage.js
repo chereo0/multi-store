d31f7b676963b1fa74e21e8be87abcfc6f4c77c4
qwerty <abdelrhmanelanani20@gmail.com>
2025-10-16 20:07:09 +0300

    fix(about): remove duplicated Navbar (App already renders global navbar)

--- mult-seller-frontend/src/pages/AboutPage.js
+++ mult-seller-frontend/src/pages/AboutPage.js
@@ -1,6 +1,5 @@
 import React from 'react';
 // Use the global Navbar (same as Home) for consistent navigation across pages
-import Navbar from '../components/Navbar';
 import AboutHero from '../components/about/AboutHero';
 import MissionSection from '../components/about/MissionSection';
 import JourneyTimeline from '../components/about/JourneyTimeline';
@@ -11,7 +10,6 @@ import CTASection from '../components/about/CTASection';
 const AboutPage = () => {
   return (
     <div className="about-page">
-  <Navbar />
       <main>
         <AboutHero />
         <MissionSection />
